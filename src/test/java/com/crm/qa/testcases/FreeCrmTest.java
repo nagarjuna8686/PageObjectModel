8abbdfd8dd85b8b9e45a0e0dfb563cee4d831466
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2020-03-15 11:53:26 +0530

    Update FreeCrmTest.java

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -20,7 +20,7 @@ public class FreeCrmTest {
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "D:\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get("https://www.freecrm.com/index.html");
@@ -83,7 +83,7 @@ public class FreeCrmTest {
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		// now copy the screenshot to desired location using copyFile //method
 		FileUtils.copyFile(src, 
-				new File("D:\Devops Softwares\Jenkins\workspace\selenium04\screenshots\" + fileName +".png"));
+				new File("D:\\Devops Softwares\\Jenkins\\workspace\\selenium04\\screenshots\\" + fileName +".png"));
 
 	}
 
