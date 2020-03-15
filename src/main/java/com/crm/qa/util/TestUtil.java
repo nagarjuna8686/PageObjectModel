2a1e1daf67ff048c786563c032b8a9617c8d4444
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2020-03-15 12:11:14 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,7 +21,7 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "D:\\Devops Softwares\\Jenkins\\workspace\\selenium04\\src\\main\\java\\com\\crm\\qa\\testdataFreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "D:\\Devops Softwares\\Jenkins\\workspace\\selenium04\\src\\main\\java\\com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
